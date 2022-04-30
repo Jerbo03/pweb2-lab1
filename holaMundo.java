360c78cac76339b1309847aedf5b65ecca8f8727
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-04-29 23:02:04 -0500

    Se añadió un método más para practicar

--- holaMundo.java
+++ holaMundo.java
@@ -2,4 +2,7 @@ class holaMundo() {
   public static void saludo() {
     System.out.println("Hola mundo"); 
   }
+  public static void saludo(String nombre) {
+    System.out.println("Hola "+nombre);
+  }
 }
